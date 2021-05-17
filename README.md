f3bc902edffe777a58d20092c4f8c0f4da4df5e3
qo-op <support@qo-op.com>
2021-05-17 03:23:01 +0200

    install uqload_downloader and detects uqload from KODI/VStream logs

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ An surprise: __error_wrong_ip__ :(
 So this software permits to skirt this inconvenience.
 
 Simply lauch __uqload_downloader.exe https://uqload.com/embed-[...].html yourmovie.mp4__ and voila
+
+---
+
+Linux users can run [./cli/download_from_kodi_log.sh](.//blob/main/cli/download_from_kodi_log.sh) (Linux Mint tested)
+to install uqload_downloader and detects uqload from KODI/VStream logs and ask for copying it to $HOME/astroport
--- cli/download_from_kodi_log.sh
+++ cli/download_from_kodi_log.sh
@@ -4,21 +4,62 @@
 # Version: 0.1
 # License: AGPL-3.0 (https://choosealicense.com/licenses/agpl-3.0/)
 ########################################################################
+# download_from_kodi_log.sh
+########################################################################
+echo "Extract uqload links from ~/.kodi/temp/kodi.${OLD}log"
+# Detects uqload links and ask for copying it to $HOME/astroport
+########################################################################
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 SCRIPT="${0##*/}"
 
+## CHECKING ENV
+[[ ! -d $HOME/astroport ]] && mkdir $HOME/astroport
+[[ ! -d $HOME/.local/bin ]] && mkdir -p $HOME/.local/bin
+[[ ! $(echo $PATH | grep "$HOME/.local/bin") ]] && export PATH="$PATH:$HOME/.local/bin"
 
-[[ ! -f $HOME/.local/bin/uqload_downloader ]]; then
-	echo "TO INSTALL PLEASE RUN"
-	echo "sudo apt-get install libcurl4-openssl-dev -y && g++ -o uqload_downloader uqload_downloader.cpp Downloader.cpp -lcurl && mv uqload_downloader $HOME/.local/bin/" 
+## CHECK BINARY / BUILD and INSTALL
+if [[ ! -f $HOME/.local/bin/uqload_downloader ]]; then
+	g++ -o uqload_downloader uqload_downloader.cpp Downloader.cpp -lcurl
+	[[ -f uqload_downloader ]] && mv uqload_downloader $HOME/.local/bin/ || echo "INSTALL FAILED, 'sudo apt-get install libcurl4-openssl-dev -y' TRY AGAIN." && exit 1
 fi
+
+## CHOOSE kodi.${OLD}log
 [[ $1 == "old" ]] && OLD='old.' || OLD=''
 
+## LOOP
 cycle=1
 for uqlink in $(cat ~/.kodi/temp/kodi.${OLD}log | grep uqload | grep 'play :' | rev | cut -d '/' -f 1 | rev);
 do
 	uqname=$(cat ~/.kodi/temp/kodi.${OLD}log | grep uqload | grep $uqlink | grep VideoPlayer | cut -d '=' -f 4 | cut -d '&' -f 1 | cut -d '%' -f 1 | sed 's/\+/_/g')
 	cycle=$((cycle+1))
+	echo "########################################################################"
 	echo "uqload_downloader https://uqload.com/$uqlink \"$HOME/astroport/$uqname.mp4\""
+	## CHECK & MANAGE COPY
+	if [[ $(find /home/fred/astroport -name "$uqname.mp4" -type f -print) ]];
+	then
+		echo "COPY ALREADY IN $HOME/astroport/"
+	else
+		echo "WATCHED MOVIE : $uqname (https://uqload.com/$uqlink)"
+		echo "WANT TO COPY ? Yes? Write any character + enter, else just hit enter."
+		read YESNO
+		if [[ "$YESNO" != "" ]]; then
+			## COPY STREAMING
+			uqload_downloader https://uqload.com/$uqlink "$HOME/astroport/$uqname.mp4"
+			## TMDB ID ?
+			echo ">>> ID TMDB? https://www.themoviedb.org/search?query=$uqname"
+			read TMDBID
+			[[ "$TMDBID" != "" ]] && mkdir -p "$HOME/astroport/film/$TMDBID/" && mv $HOME/astroport/$uqname.mp4 $HOME/astroport/film/$TMDBID/ && echo "COPY ~/astroport/$uqname.mp4 DONE" || continue
+			echo "COPY ~/astroport/film/$TMDBID/$uqname.mp4 DONE"
+		else
+			continue
+		fi
+	fi
 done
+echo 
+echo "########################################################################"
+[[ $cycle == 1 && ! ${OLD} ]] && echo "NOTHING IN CURRENT LOG, TRY old ?"
+read OLD
+[[ "$OLD" != "" ]] && $MY_PATH/$SCRIPT old
+echo "DONE"
+exit 0
